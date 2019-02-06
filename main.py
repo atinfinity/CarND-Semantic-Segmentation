4f5d577fdd7b6b319527083f68b600443693acc8
atinfinity <dandelion1124@gmail.com>
2019-02-07 00:41:49 +0900

    +code to write video file

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import os.path
 import tensorflow as tf
+import glob
 import argparse
+from moviepy.editor import ImageSequenceClip
 import helper
 import warnings
 from distutils.version import LooseVersion
@@ -26,7 +28,7 @@ def load_vgg(sess, vgg_path):
     :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
     :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
     """
-    # TODO: Implement function
+    # Implement function
     #   Use tf.saved_model.loader.load to load the model and weights
     vgg_tag = 'vgg16'
     vgg_input_tensor_name      = 'image_input:0'
@@ -191,9 +193,12 @@ def run():
                  correct_label, keep_prob, learning_rate)
 
         # Save inference data using helper.save_inference_samples
-        helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, image_input)
+        output_dir = helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, image_input)
 
         # OPTIONAL: Apply the trained model to a video
+        result_image_list = sorted(glob.glob(output_dir + '/*.png'))
+        clip = ImageSequenceClip(result_image_list, fps=5)
+        clip.write_videofile('output.mp4')
 
 
 if __name__ == '__main__':
